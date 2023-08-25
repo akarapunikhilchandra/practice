f5ff7d26e2578db834679c2fd7fa7bef166b65a4
nikhil <anc@gmail.com>
2023-08-25 12:18:18 +0530

    all

--- 02-datatypes.sh/03-float.sh
+++ 02-datatypes.sh/03-float.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# Assigning floating-point values to variables
-float_num1=2.5
-float_num2=2.5
+float_num1=3.14
+float_num2=2.71
 
-# Performing arithmetic operations
-sum=$(("float_num1 + float_num2"))
-# difference=$(echo "$float_num1 - $float_num2" | bc)
-# product=$(echo "$float_num1 * $float_num2" | bc)
-# division=$(echo "scale=2; $float_num1 / $float_num2" | bc)
+# Performing arithmetic operations using shell's built-in arithmetic
+sum=$((float_num1 + float_num2))
+difference=$((float_num1 - float_num2))
+product=$((float_num1 * float_num2))
+division=$(echo "scale=2; $float_num1 / $float_num2" | bc)
 
-# Displaying the results
 echo "Sum: $sum"
-# echo "Difference: $difference"
-# echo "Product: $product"
-# echo "Division: $division"
+echo "Difference: $difference"
+echo "Product: $product"
+echo "Division: $division"
