7d498ed24a01874b9a7d8d2de3aa328c2555b037
nikhil <anc@gmail.com>
2023-08-25 16:40:57 +0530

    all

---
+++ 05-functions.sh/01-functions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USERID=$(id -u)
+VALIDATE (){
+    if [ $1 -ne 0 ]
+then
+    echo " $2 ... FAILURE"
+    exit 1
+else 
+    echo " $2 ... SUCESS"
+fi
+}
+if [ $USERID -ne 0 ]
+then 
+    echo "ERROR :: Please Run This Sript With Root Access"
+    exit 1
+fi
+
+yum install mysql -y
+VALIDATE $? "Installing mysql"
