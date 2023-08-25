fc693c90b5601f5a06e074abd197e46f5fe560e4
nikhil <anc@gmail.com>
2023-08-25 12:15:45 +0530

    all

--- 02-datatypes.sh/03-float.sh
+++ 02-datatypes.sh/03-float.sh
@@ -5,13 +5,13 @@ float_num1=2.5
 float_num2=2.5
 
 # Performing arithmetic operations
-sum=$(echo "$float_num1 + $float_num2" | bc)
-difference=$(echo "$float_num1 - $float_num2" | bc)
-product=$(echo "$float_num1 * $float_num2" | bc)
-division=$(echo "scale=2; $float_num1 / $float_num2" | bc)
+sum=$(("float_num1 + float_num2"))
+# difference=$(echo "$float_num1 - $float_num2" | bc)
+# product=$(echo "$float_num1 * $float_num2" | bc)
+# division=$(echo "scale=2; $float_num1 / $float_num2" | bc)
 
 # Displaying the results
 echo "Sum: $sum"
-echo "Difference: $difference"
-echo "Product: $product"
-echo "Division: $division"
+# echo "Difference: $difference"
+# echo "Product: $product"
+# echo "Division: $division"
