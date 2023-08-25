a4f906d01f52e96a3ec56c0e7a096a0167d46ad3
nikhil <anc@gmail.com>
2023-08-25 13:32:29 +0530

    all

---
+++ 04-install.sh/installing.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# our program goal is to install mysql
+
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=/opt/$SCRIPT_NAME-$DATE.log
+
+# this function should validate the previous command and inform user it is success or failure
+VALIDATE(){
+    #$1 --> it will receive the argument1
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+# else
+#     echo "INFO:: You are root user"
+fi
+
+# it is our responsibility again to check installation is success or not
+yum install mysql -y &>>$LOGFILE
+
+VALIDATE $? "Installing MySQL"
