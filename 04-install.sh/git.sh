6db322465f6df2f1fcb9835e3b1ebdb7ffda972c
nikhil <anc@gmail.com>
2023-08-25 13:21:14 +0530

    all

--- 04-install.sh/git.sh
+++ 04-install.sh/git.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo "you are not root user please again try with sudo permissions"
+    exit 1
+fi
 
 # Check if Git is already installed
 if which mysql >/dev/null 2>&1; then
