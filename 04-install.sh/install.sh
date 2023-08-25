1c8ef93fb1ddfcd94b389a9fcb9f7b5889c3ab81
nikhil <anc@gmail.com>
2023-08-25 12:50:24 +0530

    all

--- 04-install.sh/install.sh
+++ 04-install.sh/install.sh
@@ -4,3 +4,11 @@ then
     echo "ERROR :: PLEASE RUN THIS SCRIPT WITH ROOT ACCESS"
     exit 1
 fi
+ 
+yum install tree -y
+if [ $? -ne 0 ]
+then 
+    echo "INSTALLATION OF TREE IS ERROR"
+else
+    echo "INSTALLATION IF TREE IS SUCCESS"
+fi
