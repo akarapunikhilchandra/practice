b03acd93ca548cec7c28a58a8e9314cc2caf4b3f
nikhil <anc@gmail.com>
2023-08-25 11:55:33 +0530

    all

--- 01-variables.sh/username.sh
+++ 01-variables.sh/username.sh
@@ -3,3 +3,7 @@
 echo "please enter your username"
 read USERNAME 
 echo "username entered is : $USERNAME"
+
+echo "please enter your password"
+read PASSWORD 
+echo "password entered is : $PASSWORD"
