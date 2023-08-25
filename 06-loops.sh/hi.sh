6455587254ae68cb962d84308c424e2572cae7d8
nikhil <anc@gmail.com>
2023-08-25 17:34:49 +0530

    all

--- 06-loops.sh/hi.sh
+++ 06-loops.sh/hi.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 DATE=$(date +%F)
 SCRIPT_NAME=$0
-LOGSDIR=/opt/logs
+LOGSDIR=/opt/logs/
 LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
 VALIDATE (){
     if [ $1 -e 0 ]
 then 
     echo "already present or installed"
+else 
+    echo "not installed"
+fi
 }
 
 yum install mysql -y &>>LOGFILE
