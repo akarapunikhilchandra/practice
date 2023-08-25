dad6a69dcb4c9d0f6464e216b11d02c1e774db04
nikhil <anc@gmail.com>
2023-08-25 18:04:47 +0530

    all

--- 06-loops/loop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DATE=$(date +%F)
-LOGSDIR=/opt/logs/
-SCRIPT_NAME=$0
-LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
-
-
-yum install tree -y &>> $LOGFILE
---
+++ 06-loops/loops.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+DATE=$(date +%F)
+LOGSDIR=/opt/logs/
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
