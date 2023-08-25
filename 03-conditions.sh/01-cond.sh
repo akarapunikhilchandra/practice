d8471410644bf105e8cc9928dd242882c23b5325
nikhil <anc@gmail.com>
2023-08-25 12:40:02 +0530

    all

--- 03-conditions.sh/01-cond.sh
+++ 03-conditions.sh/01-cond.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 NUMBER=$1
-if [ $NUMBER -le 10 ]
+if [ $NUMBER -l 10 ]
 then 
-    echo "$NUMBER is greater than or equal 10"
+    echo "$NUMBER is lesser than 10"
 else
-    echo "$NUMBER is not greater than 10"
+    echo "$NUMBER is greater than 10"
 fi
