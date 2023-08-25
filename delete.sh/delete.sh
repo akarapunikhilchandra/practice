86704242c34127939c2a75057d1ca59dbb008226
nikhil <anc@gmail.com>
2023-08-25 18:34:50 +0530

    all

---
+++ delete.sh/delete.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+APP_LOGS_DIR=/opt/logs
+
+DATE=$(date +%F:%H:%M:%S)
+LOGSDIR=/opt/logs/shellscript-logs
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
+
+FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
+
+echo "script started executing at $DATE" &>>$LOGFILE
+
+while read line
+do
+    echo "Deleting $line" &>>$LOGFILE
+    rm -rf $line
+done <<< $FILES_TO_DELETE
